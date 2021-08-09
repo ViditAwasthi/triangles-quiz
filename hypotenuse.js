bfe5d3279a37c29cab08f4a8471e1282bb7dd41f
Vidit Awasthi <viditawasthi2@gmail.com>
2021-08-09 19:55:12 +0530

    error handling

--- hypotenuse.js
+++ hypotenuse.js
@@ -3,16 +3,19 @@ const calculateBtn = document.querySelector("#check-btn")
 const outputDiv = document.querySelector("#output-div")
 
 
-function calculateSumOfSquares(a,b){
-let sumOfSquares = a*a + b*b;
-return sumOfSquares;
+function calculateSumOfSquares(a, b) {
+    let sumOfSquares = a * a + b * b;
+    return sumOfSquares;
 }
 
-function calculateHypotenuse(){
- const sumOfSquares = calculateSumOfSquares(Number(sideInputs[0].value),Number(sideInputs[1].value));
- const hypotenuseLength = Math.sqrt(sumOfSquares);
- outputDiv.innerText = "The Hypotenuse of the Triangle is: "+hypotenuseLength;
-
+function calculateHypotenuse() {
+    if (sideInputs[0].value && sideInputs[1].value) {
+        const sumOfSquares = calculateSumOfSquares(Number(sideInputs[0].value), Number(sideInputs[1].value));
+        const hypotenuseLength = Math.sqrt(sumOfSquares);
+        outputDiv.innerText = "The Hypotenuse of the Triangle is: " + hypotenuseLength;
+    } else {
+        outputDiv.innerText = "Please enter the Values!"
+    }
 }
 
 calculateBtn.addEventListener("click", calculateHypotenuse);
--- trianglearea.js
+++ trianglearea.js
@@ -3,15 +3,19 @@ const calculateBtn = document.querySelector("#check-btn")
 const outputDiv = document.querySelector("#output-div")
 
 
-function multiplyInputs(a,b){
-let product = a*b
-return product;
+function multiplyInputs(a, b) {
+    let product = a * b
+    return product;
 }
 
-function calculateArea(){
- const product = multiplyInputs(Number(sideInputs[0].value),Number(sideInputs[1].value));
- const triangleArea = 0.5*product;
- outputDiv.innerText = "The Area of Triangle is: "+triangleArea;
+function calculateArea() {
+    if (sideInputs[0].value && sideInputs[1].value) {
+        const product = multiplyInputs(Number(sideInputs[0].value), Number(sideInputs[1].value));
+        const triangleArea = 0.5 * product;
+        outputDiv.innerText = "The Area of Triangle is: " + triangleArea;
+    } else {
+        outputDiv.innerText = "Please enter the Values!"
+    }
 }
 
 calculateBtn.addEventListener("click", calculateArea);
